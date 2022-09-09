6b8237fa354dd84f54ffeb7c06cb4221703f386c
Charles Bartlett <ownageunit@gmail.com>
2022-09-09 18:26:34 -0400

    finished skewing boxes and played around with animating them

---
+++ src/components/ImageDiv.js
@@ -0,0 +1,9 @@
+const ImageDiv = ({ name }) => {
+  return (
+    <div className="ImageDiv">
+      <p>{name}</p>
+    </div>
+  );
+};
+
+export default ImageDiv;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,48 @@
+import ImageDiv from "../components/ImageDiv";
+
 const Home = () => {
+  const images = [];
+
+  const numberOfVerticalImages = 50;
+  const numberOfSkullys = 50;
+
+  for (let index = 0; index < numberOfSkullys; index++) {
+    images.push({ name: `${index}` });
+  }
+
+  const imageDivs = images.map(({ name }) => {
+    return <ImageDiv name={name} />;
+  });
+
+  const verticalImageDivs = [];
+
+  let marginTop = 0;
+
+  for (let index = 0; index < numberOfVerticalImages; index++) {
+    // if (index % 2 === 0) {
+    //   verticalImageDivs.push(
+    //     <div className="vertical-div" style={{ marginTop: "50px" }}>
+    //       {imageDivs}
+    //     </div>
+    //   );
+    // } else {
+    //   verticalImageDivs.push(<div className="vertical-div">{imageDivs}</div>);
+    // }
+
+    marginTop += 50;
+
+    verticalImageDivs.push(
+      <div className="vertical-div" style={{ marginTop: `${marginTop}px` }}>
+        {imageDivs}
+      </div>
+    );
+  }
+
+  console.log(verticalImageDivs);
+
   return (
     <div id="Home-div">
-      <h1>Home</h1>
+      <div id="InnerHome-div">{verticalImageDivs}</div>
     </div>
   );
 };

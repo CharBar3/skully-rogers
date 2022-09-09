6b70c52bb15ee17a5945715449304ff7a3fff6a8
Charles Bartlett <ownageunit@gmail.com>
2022-09-09 01:51:41 -0400

    main button styling done and nav bar skeleton done

---
+++ src/components/Nav.js
@@ -0,0 +1,25 @@
+const Nav = () => {
+  return (
+    <div id="Nav-div">
+      <h1>Skully Rogers</h1>
+      <div id="Nav-Buttons-div">
+        <nav>
+          <a>
+            <button>Home</button>
+          </a>
+          <a>
+            <button>New Arrivals</button>
+          </a>
+          <a>
+            <button>About</button>
+          </a>
+        </nav>
+        {/* <p>insta</p>
+        <p>discord</p>
+        <p>twitter</p> */}
+      </div>
+    </div>
+  );
+};
+
+export default Nav;

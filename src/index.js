c468098ee5778b1c4dfc06fc71212068fdac8757
Charles Bartlett <ownageunit@gmail.com>
2022-09-16 00:42:00 -0400

    fonts moved to public

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./styles/fonts/DINCondensed-Bold.ttf";
+// import "../public/fonts/DINCondensed-Bold.ttf";
 
 import { BrowserRouter } from "react-router-dom";
 

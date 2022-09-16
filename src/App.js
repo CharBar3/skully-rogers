18303be06662d6f14e601956c2b4787435ec73fa
Charles Bartlett <ownageunit@gmail.com>
2022-09-16 03:11:46 -0400

    mobile nav bar styling done

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import images from "./images";
 
 import { Routes, Route } from "react-router-dom";
 import NewArrivals from "./pages/NewArrivals";
+import MobileNavBar from "./components/MobileNavBar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MobileNavBar />
       <Routes>
         <Route path="/" element={<Home images={images} />} />
         <Route path="/NewArrivals" element={<NewArrivals images={images} />} />
---
+++ src/components/MobileNavBar.js
@@ -0,0 +1,20 @@
+import "../styles/componentStyles/MobileNavBar.css";
+
+const MobileNavBar = () => {
+  return (
+    <div id="MobileNavBar">
+      <div id="mobile-navbar-logo">
+        <img src="https://i.imgur.com/24OS1J3.png" alt="Skully Rogers Logo" />
+        <p>Skully Rogers</p>
+      </div>
+      <div id="mobile-navbar-menu">
+        <img
+          src="https://img.icons8.com/ios-filled/50/000000/menu--v1.png"
+          alt="Menu Icon"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MobileNavBar;
